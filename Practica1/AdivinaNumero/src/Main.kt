5079e112394cd0878c5af72a3cc2c304d5d0984d
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-08-25 16:52:34 -0500

    Subiendo logica para mayor o menor

---
+++ Practica1/AdivinaNumero/src/Main.kt
@@ -0,0 +1,31 @@
+// En este código podremos adivinar un número elegido aleatoriamente por la PC.
+// Evelyn Milagros Chipana Ramos
+// Creación: 25-08-2024
+// Finalización: 25-08-2024
+
+import kotlin.random.Random
+fun main(){
+    val numGuess = Random.nextInt(1, 31)
+    var intentos = 5
+
+    println("Adivina el número entre 1 y 30. Tienes $intentos intentos")
+    println("**SUERTE**")
+
+    while (intentos > 0){
+        println("Ingrese un número: ")
+        val trys = readLine()!!.toInt()
+        //logica para saber si es menor o mayor
+        when {
+            trys == numGuess -> {
+                println("ADIVINASTE EL NÚMERO CORRECTO :D")
+                break
+            }
+            trys < numGuess -> print("El número es mayor\n")
+            trys > numGuess -> print("El número es menor\n")
+        }
+
+    intentos--
+
+    }
+
+}
