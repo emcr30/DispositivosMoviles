e7dda7ae5f93981902a8834ee519c497d3e21a09
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-08-24 23:38:35 -0500

    Subiendo la lógica del juego
    Añadiendo when para la elección del jugador.

--- Practica1/ScissorsRock/src/Main.kt
+++ Practica1/ScissorsRock/src/Main.kt
@@ -1,11 +1,28 @@
-
+// Este código te permite jugar PIEDRA, PAPEL y TIJERA, la computadora decidirá quién ganará aleatoriamente,
+// Evelyn Milagros Chipana Ramos
+// Creación: 24-08-2024
 
 import kotlin.random.Random
 
 fun main() {
+    //creacion de un array con las opciones
     val elegir = arrayOf("piedra", "papel", "tijera")
     val PC = elegir[Random.nextInt(elegir.size)]
     print("------- Comencemos -------\n")
-    print("Elige piedra, papel o tijera:")
-    val usuario = readLine()?.lowercase()
+    print("Elige piedra, papel o tijera: ")
+    //asegurar que el jugador ingrese datos
+    val jugador = readLine()?.lowercase()
+
+    when {
+        jugador == PC -> println("¡Empate!")
+        //comprueba que el jugador gana con la eleccion que hizo
+        jugador == "piedra" && PC == "tijera" ||
+                jugador == "papel" && PC == "piedra" ||
+                jugador == "tijera" && PC == "papel" -> println("¡GANASTE! :D")
+        //verificar que la eleccion sea válida
+        jugador in elegir -> println("¡PERDISTE! :( LA PC ELIGIÓ: $PC")
+        //por si no coincide la entrada del jugador
+        else -> println("Opción no válida")
+    }
 }
+
