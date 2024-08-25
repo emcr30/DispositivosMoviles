48d3c776f73b7afb9e66b36108a196ba0c4347df
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-08-25 12:02:07 -0500

    Subiendo la funcion con los datos y el calculo de rendimiento

--- Practica1/EvEmpleados/src/Main.kt
+++ Practica1/EvEmpleados/src/Main.kt
@@ -1,14 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+//EVALUACIÓN DE EMPLEADOS: Se calificará a los empleados con un puntaje de 0 a 10, para con esto calcular su sueldo por su rendimiento.
+//// Evelyn Milagros Chipana Ramos
+//// Creación: 25-08-2024
+//// Finalización: 25-08-2024
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main(){
+    //ingresar los datos del empleado
+    print("Ingrese su nombre: ")
+    val nombre = readLine()
+
+    print("Ingrese su puntuación: ")
+    val puntuacion = readLine().toInt()
+
+    print("Ingrese su salario: ")
+    val salario = readLine().toDouble()
+
+    //calcular el rendimiento
+    val rendimiento = when (puntuacion){
+        in 0..3 -> "Inaceptable"
+        in 4..6 -> "Aceptable"
+        in 7..10 -> "Meritorio"
     }
+
 }
