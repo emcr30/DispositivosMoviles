a33250d843ce16e6d75017034bbf0801b9a4ba16
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-08-24 23:54:35 -0500

    Añadiendo un loop
    De esta manera el jugador decidirá cuando dejar de jugar.

--- Practica1/ScissorsRock/src/Main.kt
+++ Practica1/ScissorsRock/src/Main.kt
@@ -1,4 +1,4 @@
-// Este código te permite jugar PIEDRA, PAPEL y TIJERA, la computadora decidirá quién ganará aleatoriamente,
+// Este código te permite jugar PIEDRA, PAPEL y TIJERA, la computadora decidirá quién ganará aleatoriamente.
 // Evelyn Milagros Chipana Ramos
 // Creación: 24-08-2024
 
@@ -7,22 +7,30 @@ import kotlin.random.Random
 fun main() {
     //creacion de un array con las opciones
     val elegir = arrayOf("piedra", "papel", "tijera")
-    val PC = elegir[Random.nextInt(elegir.size)]
-    print("------- Comencemos -------\n")
-    print("Elige piedra, papel o tijera: ")
-    //asegurar que el jugador ingrese datos
-    val jugador = readLine()?.lowercase()
+    while (true) {
+        val PC = elegir[Random.nextInt(elegir.size)]
+        print("-----------------¡A JUGAR! -----------------\n")
+        print("NOTA : Escribe 'salir' si quieres abandonar el juego.\n")
+        print("*****COMENCEMOS*****\n")
+        print("Elige piedra, papel o tijera: ")
+        //asegurar que el jugador ingrese datos correctos
+        val jugador = readLine()?.lowercase()
 
-    when {
-        jugador == PC -> println("¡Empate!")
-        //comprueba que el jugador gana con la eleccion que hizo
-        jugador == "piedra" && PC == "tijera" ||
-                jugador == "papel" && PC == "piedra" ||
-                jugador == "tijera" && PC == "papel" -> println("¡GANASTE! :D")
-        //verificar que la eleccion sea válida
-        jugador in elegir -> println("¡PERDISTE! :( LA PC ELIGIÓ: $PC")
-        //por si no coincide la entrada del jugador
-        else -> println("Opción no válida")
+        if (jugador == "salir") {
+            println("Gracias por jugar. ¡Nos vemos pronto!")
+            break // salir del bucle si el jugador elige salir
+        }
+
+        when {
+            jugador == PC -> println("¡Empate!")
+            //comprueba que el jugador gana con la eleccion que hizo
+            jugador == "piedra" && PC == "tijera" ||
+                    jugador == "papel" && PC == "piedra" ||
+                    jugador == "tijera" && PC == "papel" -> println("¡GANASTE! :D")
+            //verificar que la eleccion sea válida
+            jugador in elegir -> println("¡PERDISTE! :( LA PC ELIGIÓ: $PC")
+            //por si no coincide la entrada del jugador
+            else -> println("Opción no válida")
+        }
     }
 }
-
