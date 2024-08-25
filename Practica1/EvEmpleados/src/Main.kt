db52b2379676c9e2446e2cbb660b246cc9705fe2
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-08-25 12:29:47 -0500

    Subiendo el calculo del sueldo del empleado

--- Practica1/EvEmpleados/src/Main.kt
+++ Practica1/EvEmpleados/src/Main.kt
@@ -8,17 +8,26 @@ fun main(){
     print("Ingrese su nombre: ")
     val nombre = readLine()
 
-    print("Ingrese su puntuación: ")
-    val puntuacion = readLine().toInt()
+    print("Ingrese su puntuación (0 a 10): ")
+    val puntuacion = readLine()?.toIntOrNull() ?: return println("Puntuación inválida")
 
     print("Ingrese su salario: ")
-    val salario = readLine().toDouble()
+    val salario = readLine()?.toDoubleOrNull() ?: return println("Salario inválido")
 
-    //calcular el rendimiento
-    val rendimiento = when (puntuacion){
+    // Calcular el rendimiento
+    val rendimiento = when (puntuacion) {
         in 0..3 -> "Inaceptable"
         in 4..6 -> "Aceptable"
         in 7..10 -> "Meritorio"
+        else -> "Rango NO ACEPTADO"
     }
+
+    //calcular sueldo
+    val sueldo = if (puntuacion in 0..10) salario * (puntuacion / 10.0) else 0.0
+
+    // Imprimir resultados
+    println("EL EMPLEADO: $nombre")
+    println("NIVEL DE RENDIMIENTO: $rendimiento")
+    println("SUELDO DEL MES: $${"%.2f".format(sueldo)}")
 
 }
