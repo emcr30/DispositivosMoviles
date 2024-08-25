df8d25a76af2a56064dea897354035272cb4ff3f
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-08-25 17:22:00 -0500

    Agregando comentarios

--- Practica1/AdivinaNumero/src/Main.kt
+++ Practica1/AdivinaNumero/src/Main.kt
@@ -4,17 +4,22 @@
 // Finalización: 25-08-2024
 
 import kotlin.random.Random
+
 fun main(){
+
+    //genera un numero aleatorio entre 1 y 30
     val numGuess = Random.nextInt(1, 31)
-    var intentos = 5
+    var intentos = 5 //intentos permitidos
 
     println("Adivina el número entre 1 y 30. Tienes $intentos intentos")
     println("**SUERTE**")
 
+    //comienzo del bucle para varios intentos
     while (intentos > 0){
         println("Ingrese un número: ")
         val trys = readLine()!!.toInt()
-        //logica para saber si es menor o mayor
+
+        //logica para saber si la entrada es menor o mayor
         when {
             trys == numGuess -> {
                 println("ADIVINASTE EL NÚMERO CORRECTO :D")
@@ -25,6 +30,7 @@ fun main(){
         }
 
         intentos--
+
         //cantidad de intentos y si la respuesta es incorrecta
         if (intentos > 0){
             println("Te quedan $intentos intentos")
