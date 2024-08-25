3fe6d28b8dff640e620db4c0a37cca5ac59b4df9
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-08-25 16:36:49 -0500

    Corrigiendo la division para calcular con double

--- Practica1/Calculadora/src/Main.kt
+++ Practica1/Calculadora/src/Main.kt
@@ -3,55 +3,62 @@
 // Creación: 25-08-2024
 // Finalización: 25-08-2024
 
-fun main(){
-    //control del loop del menú
+fun main() {
+    // Control del loop del menú
     var calculadora = true
 
-    while(calculadora){
+    while (calculadora) {
         println("************ MENÚ ************ ")
         println("1. Suma")
         println("2. Resta")
-        println("3. Multiplicacion")
-        println("4. Division")
+        println("3. Multiplicación")
+        println("4. División")
         println("5. Salir")
         println("Elige una opción del 1 al 5: ")
 
-        //leer la opcion del usuario
+        // Leer la opción del usuario
         val opcion = readln().toIntOrNull()
 
-        //elegir opciones
+        // Elegir opciones
         when (opcion) {
             1 -> Operacion("suma")
             2 -> Operacion("resta")
-            3 -> Operacion("multiplicacion")
-            4 -> Operacion("division")
+            3 -> Operacion("multiplicación")
+            4 -> Operacion("división")
             5 -> {
                 println("SALIENDO DE LA CALCULADORA")
                 calculadora = false
             }
-            else -> println("Opcion no válida. Elige del 1 al 5")
+            else -> println("Opción no válida. Elige del 1 al 5")
         }
     }
 }
 
-//ingresar los números
-fun Operacion(type: String){
+// Ingresar los números
+fun Operacion(type: String) {
     print("Ingrese el primer número: ")
-    val num1 = readln().toIntOrNull() ?: run {
+    val num1 = readln().toDoubleOrNull() ?: run {
         println("Ingreso no válido. Tiene que ser un número.")
         return
     }
     print("Ingrese el segundo número: ")
-    val num2 = readln().toIntOrNull() ?: run {
+    val num2 = readln().toDoubleOrNull() ?: run {
         println("Ingreso no válido. Tiene que ser un número.")
         return
     }
 
-    //realizando las operaciones
-    when(type) {
-        "suma" -> println(num1 + num2)
-        "resta" -> println(num1 - num2)
-        "multiplicacion" -> println(num1 * num2)
-        "division" -> println(num1 / num2)
+    // Realizando las operaciones
+    when (type) {
+        "suma" -> println("RESPUESTA: ${num1 + num2}")
+        "resta" -> println("RESPUESTA: ${num1 - num2}")
+        "multiplicación" -> println("RESPUESTA: ${num1 * num2}")
+        "división" -> {
+            if (num2 != 0.0) { // verificar double
+                val respuesta = num1 / num2
+                println("RESPUESTA: $respuesta")
+            } else {
+                println("No se puede dividir entre cero")
+            }
+        }
     }
 }
