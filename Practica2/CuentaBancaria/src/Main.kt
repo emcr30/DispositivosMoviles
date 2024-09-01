277408d95d183e6fec6f5cdf58738af9ca3e1762
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-09-01 08:22:16 -0500

    Subiendo set y get

---
+++ Practica2/CuentaBancaria/src/Main.kt
@@ -0,0 +1,36 @@
+// Creación de una cuenta bancaria con saldo y un límite de retiro
+// Evelyn Milagros Chipana Ramos
+// Creación: 01-09-2024
+// Finalización:
+
+import java.util.Scanner
+
+//creacion de la clase cuenta bancaria
+class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double) {
+    //implementacion de set y get 
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(nuevoSaldo: Double) {
+        if (nuevoSaldo >= 0) {
+            saldo = nuevoSaldo
+        } else {
+            println("El saldo no puede ser negativo.")
+        }
+    }
+
+    fun getLimiteRetiro(): Double {
+        return limiteRetiro
+    }
+
+    fun setLimiteRetiro(nuevoLimite: Double) {
+        if (nuevoLimite >= 0) {
+            limiteRetiro = nuevoLimite
+        } else {
+            println("El límite de retiro no puede ser negativo.")
+        }
+    }
+}
+
+
