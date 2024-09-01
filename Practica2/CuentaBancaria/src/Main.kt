474b57e5f9a97790cf69a91a63e67d86418ea926
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-09-01 08:49:23 -0500

    subiendo funcion retirar

--- Practica2/CuentaBancaria/src/Main.kt
+++ Practica2/CuentaBancaria/src/Main.kt
@@ -4,15 +4,14 @@
 // Finalización:
 
 import java.util.Scanner
-
-//creacion de la clase cuenta
+//creacion de la clase cuenta bancaria
 class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double) {
     //implementacion de set y get
-    fun getSaldo(): Double {
+    fun getSaldo(): Double { //saldo actual
         return saldo
     }
 
-    fun setSaldo(nuevoSaldo: Double) {
+    fun setSaldo(nuevoSaldo: Double) { //nuevo saldo
         if (nuevoSaldo >= 0) {
             saldo = nuevoSaldo
         } else {
@@ -20,17 +19,34 @@ class CuentaBancaria(private var saldo: Double, private var limiteRetiro: Double
         }
     }
 
-    fun getLimiteRetiro(): Double {
+    fun getLimiteRetiro(): Double { //limite de retiro
         return limiteRetiro
     }
 
-    fun setLimiteRetiro(nuevoLimite: Double) {
+    fun setLimiteRetiro(nuevoLimite: Double) { //nuevo limite de retiro
         if (nuevoLimite >= 0) {
             limiteRetiro = nuevoLimite
         } else {
             println("El límite de retiro no puede ser negativo.")
         }
     }
+
+    fun retirar(cantidad: Double): Boolean { //retiro de la cuenta
+        return if (cantidad > limiteRetiro) {
+            println("La cantidad excede el límite de retiro.")
+            false
+        } else if (cantidad > saldo) {
+            println("Saldo insuficiente.")
+            false
+        } else {
+            saldo -= cantidad
+            println("Retiro exitoso. Nuevo saldo: $$saldo")
+            true
+        }
+    }
 }
+
+
+
 
 
