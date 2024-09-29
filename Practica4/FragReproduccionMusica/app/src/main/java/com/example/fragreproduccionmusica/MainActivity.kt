93087673aba531d993dbfb30e2db0f3c34454334
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-09-28 22:56:31 -0500

    P4: Creando el proyecto ReproduccionMusica con Fragments

---
+++ Practica4/FragReproduccionMusica/app/src/main/java/com/example/fragreproduccionmusica/MainActivity.kt
@@ -0,0 +1,43 @@
+// Implementación de fragments en un reproductor de musica
+// Evelyn Milagros Chipana Ramos
+// Creación: 22-09-2024
+// Finalización: 28-09-2024
+
+package com.example.fragreproduccionmusica
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+
+
+class MainActivity : AppCompatActivity(), AudioSelectionFragment.OnAudioSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // fragmento de selección de audio
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, AudioSelectionFragment().apply {
+                    setOnAudioSelectedListener(this@MainActivity)
+                })
+            }.commit()
+        }
+    }
+
+    override fun onAudioSelected(audioName: String) {
+        // reproductor de audio con el audio seleccionado
+        val audioPlayerFragment = AudioPlayerFragment().apply {
+            arguments = Bundle().apply {
+                putString("audio_name", audioName)
+            }
+        }
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, audioPlayerFragment)
+            addToBackStack(null) // regresar al fragmento anterior
+        }.commit()
+
+    }
+}
