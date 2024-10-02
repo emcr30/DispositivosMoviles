07b475e1eefaa27451f0db046e72436f117525e0
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-10-01 21:28:26 -0500

    la clase Audio creada

---
+++ Practica5/RecyclerViewReproMusic/app/src/main/java/com/example/recyclerviewrepromusic/Audio.kt
@@ -0,0 +1,12 @@
+// Clase Audio, para el modelo de datos
+// Evelyn Milagros Chipana Ramos
+// Creación: 01-10-2024
+// Finalización: 01-10-2024
+package com.example.recyclerviewrepromusic
+
+class Audio {
+    val name: String,
+    val imageResId: Int,
+    val audioResId: Int,
+    val duration: String
+}
