281837717365131581a3038a0dea5178b6e2a1ea
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2024-10-01 21:49:57 -0500

    Activity main, en el proyecto

--- Practica5/RecyclerViewReproMusic/app/src/main/java/com/example/recyclerviewrepromusic/MainActivity.kt
+++ Practica5/RecyclerViewReproMusic/app/src/main/java/com/example/recyclerviewrepromusic/MainActivity.kt
@@ -1,20 +1,23 @@
+// Activity principal
+// Evelyn Milagros Chipana Ramos
+// Creación: 01-10-2024
+// Finalización: 01-10-2024
+
 package com.example.recyclerviewrepromusic
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, AudioListFragment())
+                .commit()
         }
     }
 }
